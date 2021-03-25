35441843638d031c9f2b6faca4a55afa87b7d8be
Vsevolod <aleksandriyskiy99@mail.ru>
2021-03-26 00:06:34 +0300

    add 3 files

---
+++ Task1.py
@@ -0,0 +1,15 @@
+# Play store вернул данные с "мусором". Мусором являются единицы.
+# Исправьте приведенный ниже код таким образом чтобы все условия выполнялись без ошибок.
+
+parsed_data = [100, 1, 5, 20, 1, 25, 1, 55, 75, 1, 1, 1]
+
+while 1 in parsed_data:
+    parsed_data.remove(1)
+else:
+    parsed_data.sort()
+
+    print (parsed_data)
+# Ниже набор тестов, менять нельзя
+assert parsed_data.count(1) == 0
+assert len(parsed_data) == 6
+assert parsed_data == [5, 20, 25, 55, 75, 100]
