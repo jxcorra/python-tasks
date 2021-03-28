98f5c6d594d746407a744d9b9e4f2832a5ec1a27
rshyrko <raman.shyrko@splitmetrics.com>
2021-03-28 11:41:55 +0300

    Add is less than function

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ def is_all_less_than(sequence, bound):
     assert all(isinstance(item, int) for item in sequence)
 
     return all(item < bound for item in sequence)
+
+
+def is_less_than(item, bound):
+    return item < bound
