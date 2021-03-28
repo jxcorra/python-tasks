5ae683876720948e0ad7844312cf19ecca58bee2
rshyrko <raman.shyrko@splitmetrics.com>
2021-03-28 11:32:50 +0300

    Add asserts

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def print_hello(name):
+    assert isinstance(name, str)
+
     print('Hello ' + name)
 
 
 def is_even(number):
+    assert isinstance(number, int)
+
     return number % 2 == 0
 
 
 def is_all_less_than(sequence, bound):
+    assert all(isinstance(item, int) for item in sequence)
+
     return all(item < bound for item in sequence)
