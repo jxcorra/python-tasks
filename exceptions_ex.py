2df6c5fd027a0610feafa7ae3d1cd35d71f0ef3a
rshyrko <raman.shyrko@splitmetrics.com>
2021-04-04 23:05:29 +0300

    Add doctests

--- exceptions_ex.py
+++ exceptions_ex.py
@@ -1,41 +1,39 @@
+import doctest
 import re
 from typing import Optional
 
 
-class MyOwnException(Exception):
-    pass
-
-
-input_data = 'fc86f9cbcaf64562852294bf21992aa27c5c3e219ce141dd84f0b54348080b34a1defb854542463f88e1b9cd88f31e28'
-
-
-# спрашиваем разрешения
 def get_safe_if(from_str: str, regexp: str, group: Optional[int] = None) -> Optional[str]:
-    """Returns matched `group` (optional) or match from `from_str` specified by `regexp`"""
-    if match := re.search(regexp, from_str).group(group or 0):
-        return match
+    """
+    Returns matched `group` (optional) or match from `from_str` specified by `regexp`
+    >>> get_safe_if('abc', 'qwe')
+    >>> get_safe_if('abc', 'qwe', 1)
+    >>> get_safe_if('abc', 'a(bc)', 1)
+    'bc'
+    >>> get_safe_if('abc', 'a(bc)')
+    'abc'
+    """
+    if match := re.search(regexp, from_str):
+        return match.group(group or 0)
 
     return None
 
 
-digits_after_two_a = (get_safe_if(input_data, r'aa(\d+)', 1))
-
-
-assert int(digits_after_two_a) == 27
-
-
-# извиняемся если что-то пошло не так
 def get_safe_exc(from_str: str, regexp: str, group: Optional[int] = None) -> Optional[str]:
-    """Returns matched `group` (optional) or match from `from_str` specified by `regexp`"""
+    """
+    Returns matched `group` (optional) or match from `from_str` specified by `regexp`
+    >>> get_safe_exc('abc', 'qwe')
+    >>> get_safe_exc('abc', 'qwe', 1)
+    >>> get_safe_exc('abc', 'a(bc)', 1)
+    'bc'
+    >>> get_safe_exc('abc', 'a(bc)')
+    'abc'
+    """
     try:
         return re.search(regexp, from_str).group(group or 0)
     except AttributeError:
         return None
 
 
-digits_after_three_a = (get_safe_exc(input_data, r'aaa(\d+)', 1))
-assert digits_after_three_a is None
-
-
-data_without_zeroes = re.sub('0', '', input_data)
-assert '0' not in data_without_zeroes
+if __name__ == '__main__':
+    doctest.testmod()
