2b74d4989a1cd4c0a0c4a5a8a0d3e483299e97b0
Kseniya-Lepilova <70340759+Kseniya-Lepilova@users.noreply.github.com>
2021-04-05 18:50:21 +0300

    fix HW2

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ parsed_data = [100, 1, 5, 20, 1, 25, 1, 55, 75, 1, 1, 1]
 while 1 in parsed_data:
     parsed_data.remove(1)
 
-    parsed_data.sort()
-
-    print(parsed_data)
+parsed_data.sort()
+print(parsed_data)
 
     # Ниже набор тестов, менять нельзя
 assert parsed_data.count(1) == 0
@@ -57,6 +56,6 @@ user = {
 print(user.keys())
 
 # Вторая строка
-print(user['first_name'], user['id'], user['city'])
-# OR
 print(user.values())
+
+
